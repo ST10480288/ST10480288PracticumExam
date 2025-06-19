ba816345fe538e2a677e7c6bcb40d517affafac6
ST10480288 <ST10480288@vcconnect.edu.za>
2025-06-19 10:15:27 +0200

    SECOND COMMIT Created songsActivity.kt and activity_songs.xml, made the screen lookm presentable, all coded for the ratings and calculations are put in

--- app/src/main/java/za/co/varsitycollege/st10480288/st10480288practicumexam/songsActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10480288/st10480288practicumexam/songsActivity.kt
@@ -16,5 +16,16 @@ class songsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val questions = arrayOf(
+        "Humble - Kendrick Lamar - Rap"
+        "Blessings - Calvin Harris - Dance Song"
+        "Summer too Hot - Chris Brown - Best Love Song"
+        "Yellow - ColdPlay - Memories"
+        )
+
+        val answers = booleanArrayOf(4, 1, 2, 3)
+
+
+
     }
 }
